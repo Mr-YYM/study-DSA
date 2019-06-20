0af068567aef9ea2ef0761550b4e8d4f7c34dd93
yeming <492851378@qq.com>
2019-06-21 01:42:56 +0800

    03-QUEUE：correct format

--- 03-Stacks-and-Queues/04-Loop-Queue/Java/src/Queue.java
+++ 03-Stacks-and-Queues/04-Loop-Queue/Java/src/Queue.java
@@ -2,6 +2,6 @@ public interface Queue<E> {
     int getSize();
     boolean isEmpty();
     void enQueue(E e);
-         E getFront();
-         E deQueue();
-         }
+    E getFront();
+    E deQueue();
+}
