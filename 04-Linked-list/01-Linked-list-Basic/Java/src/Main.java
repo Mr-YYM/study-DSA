a7ee4b6824333b80eeac3c57f6f8d04920b2678f
yeming <492851378@qq.com>
2019-06-21 13:38:43 +0800

    04-Linked-List: 为链表实现测试用例

--- 04-Linked-list/01-Linked-list-Basic/Java/src/Main.java
+++ 04-Linked-list/01-Linked-list-Basic/Java/src/Main.java
@@ -1,12 +1,20 @@
 public class Main{
     public static void main(String[] args) {
         LinkedList<Integer> linkedlist = new LinkedList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             linkedlist.addFirst(i);
             System.out.println(linkedlist);
         }
 
         linkedlist.add(2, 666);
         System.out.println(linkedlist);
+
+        linkedlist.remove(2);
+        System.out.println(linkedlist);
+
+        linkedlist.removeFirst();
+        linkedlist.removeLast();
+        System.out.println(linkedlist);
+
     }
 }
