e8e01dd0b9b231d8a6e7c64bb51cbb79bf5b810d
颜业明 <yanyeming@yanyemingdeMacBook-Pro.local>
2019-07-29 13:31:00 +0800

    add comment

--- 05-sort/01-bubble-sort/python/solution.py
+++ 05-sort/01-bubble-sort/python/solution.py
@@ -1,6 +1,7 @@
 # 冒泡排序，自底向上，倒序排列
 def bubble_sort(list):
     for i in range(len(list)-1):
+        # range(len(list)-1, i, -1) 将倒数第一个数往上冒，j 直到排好序的数后末尾两个
         for j in range(len(list)-1, i, -1):
             if list[j] > list[j-1]:
                 list[j], list[j-1] = list[j-1], list[j]
